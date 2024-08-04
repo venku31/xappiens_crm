8713002f8b70bdb506ac38e9f17f20a74a9775cc
aashishvashihst6 <aashishvashihst6@gmail.com>
2024-08-04 14:24:31 +0530

    fixes

--- xappiens_crm/xappiens_crm/doc_events/lead.py
+++ xappiens_crm/xappiens_crm/doc_events/lead.py
@@ -9,17 +9,30 @@ def serialize_datetime(obj):
 
 @frappe.whitelist()
 def execute(doc, method=None):
-    lead = frappe.db.sql("""
+    old_lead = frappe.db.sql("""
         SELECT * FROM `tabCRM Lead`
         WHERE email = '{}'
     """.format(doc.email),as_dict=1)
     
-    if lead and lead[0]['name']!=doc.name:
-        serialized_lead = json.dumps(lead, default=serialize_datetime)
+   
+    
+    if old_lead and old_lead[0]['name']!=doc.name:
+        serialized_lead = json.dumps(old_lead, default=serialize_datetime)
         doc.append("custom_leads", {
                             "lead_data_json": serialized_lead
                         })
+        lead_child =frappe.db.sql("""
+        SELECT lc.lead_data_json FROM `tabLead Child` lc,`tabCRM Lead` l
+        WHERE lc.parent = l.name and l.name = '{}'
+    """.format(old_lead[0]['name']),as_dict=1)
+        
+
+        if lead_child:
+            for lead in lead_child:
+                doc.append("custom_leads", {
+                                    "lead_data_json": lead.get('lead_data_json')
+                                })
     
-        frappe.delete_doc("CRM Lead", lead[0]['name'], ignore_missing=True)
+        frappe.delete_doc("CRM Lead", old_lead[0]['name'], ignore_missing=True)
         
     
